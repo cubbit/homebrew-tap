7bdb616a677b2fb10555499ce673262c8383b334
Marco Moschettini <marco.moschettini@cubbit.io>
2021-02-22 20:29:24 +0100

    feat: add Cubbit tap

---
+++ Casks/cubbit.rb
@@ -0,0 +1,11 @@
+cask "cubbit" do
+  version "8.0.2"
+  sha256 "4f080e5ef4679f1baa371c7d58174af430878a5c29f72ab7758a733287773c8b"
+
+  url "https://get.cubbit.io/desktop/mac/Cubbit-#{version}.dmg"
+  name "Cubbit"
+  desc "Sync & Share application for the distributed cloud"
+  homepage "https://www.cubbit.io/"
+
+  app "Cubbit.app"
+end
---
+++ README.md
@@ -0,0 +1,9 @@
+# Cubbit Tap
+
+## How do I install these formulae?
+`brew install cubbit/tap/<formula>`
+
+Or `brew tap cubbit/tap` and then `brew install <formula>`.
+
+## Documentation
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
