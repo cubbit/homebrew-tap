8688223e310e286bc1356a3b9f4620078fe4cba2
lorenzo cazzoli <lurenz.sgp@gmail.com>
2022-05-24 17:23:47 +0200

    fix: sha256sum

--- Casks/cubbit.rb
+++ Casks/cubbit.rb
@@ -1,6 +1,6 @@
 cask "cubbit" do
   version "9.2.6"
-  sha256 "ea5fad7d95530bb08c062dec44bb8c710067f90f090970526d07aa525bdb33cf"
+  sha256 "25ef59311f5d1bb86041a4f1e94dd5944aa84651e7a156e8a4461de0a123d738"
 
   url "https://get.cubbit.io/desktop/mac/Cubbit-#{version}.dmg"
   name "Cubbit"
